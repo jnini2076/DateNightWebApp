933bd237d4d994585728ad95545a66501e7942fc
jnini2076 <jonathan.padillaa17@gmail.com>
2026-07-20 15:11:52 -0400

    cleaned project and initalized routing

---
+++ FE/src/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <Home />,
+  },
+]);
+
+export default router;
